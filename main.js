a4339505dafc47d5f8784aca7a90ec020d4b5308
Johann Kaercher <jkaercher@stud.hs-heilbronn.de>
2023-05-17 17:32:12 +0200

    login as a form

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 let SQLQueryBad = require('./public/js/dbUserSearch');
 
 const express = require('express');
+const bodyParser = require("express");
 const app = express();
 const port = 80;
 let query = new SQLQueryBad();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, function(){
     console.log("Server started");
+})
+
+app.post("/account", (req,res) => {
+    let data = req.body;
+    console.log("Login Daten: " + req.body);
+    res.send({foo : 'bar'})
 })
