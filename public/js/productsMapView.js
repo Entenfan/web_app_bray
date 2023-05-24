402494d57045ebdd64143e8615b19e06006454a8
Johann Kaercher <jkaercher@stud.hs-heilbronn.de>
2023-05-24 23:10:26 +0200

    upsi, wichtige klasse vergessen uwu

---
+++ public/js/productsMapView.js
@@ -0,0 +1,16 @@
+let planArray = new Array();
+planArray = ["Test", "Boeser Plan"];
+function updateNameDisplay() {
+    if(document.getElementById("input-field")&& document.getElementById("input-field").value) {
+        planArray.push(document.getElementById('input-field').value);
+    }
+        let todo = " <div id=\"checklist\">";
+        for (let i = 0; i < planArray.length; i++) {
+            todo += '<input checked="" value="1" name="r" type="checkbox" id="01">' +
+                '<label htmlFor="01" style="color: #4f1c21;">' + planArray[i] + '</label>';
+            console.log(todo)
+        }
+        todo += "</div>"
+        document.getElementById('plan').innerHTML = todo;
+}
+
