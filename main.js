4a32bc9dcb587fde2723e4e4c29a6c82656cec6d
Entenfan <ultra_ente@yahoo.de>
2023-06-07 22:48:32 +0200

    ja keine ahnung

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ const express = require('express');
 const app = express();
 const port = 80;
 const mysql = require('mysql');
+const md5 = require('md5');
+const { htmlToText } = require('html-to-text');
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-var con = mysql.createConnection({
+
+const con = mysql.createConnection({
     host: "localhost",
-    user: "root",
+    user: "webshop",
     password: "password",
     database: "webshop"
 });
@@ -17,12 +20,27 @@ var con = mysql.createConnection({
 app.listen(port, function(){
     console.log("Server started");
 })
-app.set('view engine', 'ejs');
 
-app.post("/account", (req,res) => {
+// SQL Injection wird jetzt auf Admin-Seite durchgefuehrt, wo ausversehen ein User drin ist mit Kommentar, muss in "user" schema (Hinweis auf anderes Schema)
+app.post("/account", async (req, res) => {
+    if (correctEmailXSS(req.body.loginEmail)) {
+        await new Promise(r => setTimeout(r, 5000));
+        res.send({email: "guy1@badguys.com", password: "iluvmumbai"})
+    }
+    else if (req.body.loginEmail === "guy1@badguys.com" && req.body.loginPassword === "iluvmumbai"){
+        console.log("Erfolgreich als Admin eingeloggt");
+        res.cookie('privilege', 'Admin');
+        res.status(302).redirect('http://localhost/adminPage.html');
+    }
+    else {
+        res.status(302).redirect('http://localhost/account.html');
+    }
+})
+
+app.post("/product", (req,res) => {
     console.log("Login Daten: " + req.body.loginUsername + " " + req.body.loginPassword);
     // noinspection JSStringConcatenationToES6Template,SqlResolve
-    let queryString = "select * from users Where username ='" + req.body.loginUsername + "' AND password = '" + req.body.loginPassword + "'";
+    let queryString = "select * from users Where username ='" + req.body.loginUsername + "' AND password = '" + md5(req.body.loginPassword) + "'";
     console.log(queryString);
     try {
         con.query(queryString, function(err, result) {
@@ -50,3 +68,8 @@ app.post("/adminPage", function(req,res) {
     console.log(req.body.searchBarInput);
     //planArray.push(req.body.searchBarInput);
 })
+
+function correctEmailXSS(email) {
+    let xssEmailInput = email.split(/,(.*)/s);
+    return xssEmailInput[0] === "guy1@badguys.com" && xssEmailInput[1].includes('<a href="http://attacker.com">') && xssEmailInput[1].includes('</a>')
+}
