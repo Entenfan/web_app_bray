d075dde0a1d0cacf16cb17ab1b08f5c834b8930a
Entenfan <ultra_ente@yahoo.de>
2023-06-13 21:36:20 +0200

    joa geht bisschen

--- public/js/productsView.js
+++ public/js/productsView.js
@@ -1,21 +1,19 @@
 function updateProductsDisplay(data) {
-    console.log(JSON.parse(data));
     let mydata = JSON.parse(data)
-    for (let i = 0; i < mydata.length; i++) {
-        if (mydata[i].name) {
-            alert(mydata[i].name);
-        } else {
-            alert(mydata[i].email);
+    let todo = " <div id=\"checklist\">";
+    if (data && data.length > 0) {
+        for (let i = 0; i < mydata.length; i++) {
+            if (mydata[i].name) {
+                todo += '<input value="0" name="r" type="checkbox" id="01">' +
+                    '<label htmlFor="01" style="color: #4f1c21;">' + mydata[i].name + '</label>';
+                console.log(todo)
+            } else {
+                todo += '<input value="0" name="r" type="checkbox" id="01">' +
+                    '<label htmlFor="01" style="color: #4f1c21;">' + mydata[i].email + '</label>';
+                console.log(todo)
+            }
         }
     }
+    todo += "</div>"
+    document.getElementById('search').innerHTML = todo;
 }
-// let todo = " <div id=\"checklist\">";
-// if (data && data.length > 0) {
-//     for (let i = 0; i < data.length; i++) {
-//         todo += '<input checked="" value="0" name="r" type="checkbox" id="01">' +
-//             '<label htmlFor="01" style="color: #4f1c21;">' + data[i].name + '</label>';
-//         console.log(todo)
-//     }
-// }
-// todo += "</div>"
-// document.getElementById('search').innerHTML = todo;
