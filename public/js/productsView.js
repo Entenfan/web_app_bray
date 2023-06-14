d756f28057dca5c04afd09b33f73725b4c6ba967
Entenfan <ultra_ente@yahoo.de>
2023-06-14 09:01:53 +0200

    Formatierung angepasst und code cleanup

--- public/js/productsView.js
+++ public/js/productsView.js
@@ -5,18 +5,18 @@ function updateProductsDisplay(data) {
         for (let i = 0; i < mydata.length; i++) {
             if (mydata[i].name) {
                 todo += '<input value="0" name="r" type="checkbox" id="01">' +
-                    '<label htmlFor="01" style="color: #4f1c21;">' + mydata[i].name + '</label>';
+                    '<label htmlFor="01" style="color: #4f1c21;">' + mydata[i].name +'| ' + mydata[i].beschreibung + '| ' + mydata[i].location + '</label>';
             } else if (mydata[i].email) {
                 todo += '<input value="0" name="r" type="checkbox" id="01">' +
-                    '<label htmlFor="01" style="color: #4f1c21;">' + mydata[i].email + '</label>';
+                    '<label htmlFor="01" style="color: #4f1c21;">' + mydata[i].email +'| '+ mydata[i].password + '| ' + mydata[i].comment +'</label>';
             }
             for (let j = 0; j<mydata[i].length; j++) {
                 if (mydata[i][j].name) {
                     todo += '<input value="0" name="r" type="checkbox" id="01">' +
-                        '<label htmlFor="01" style="color: #4f1c21;">' + mydata[i][j].name + '</label>';
+                        '<label htmlFor="01" style="color: #4f1c21;">' + mydata[i][j].name + '| ' + mydata[i][j].beschreibung + '| ' + mydata[i][j].location +'</label>';
                 } else if (mydata[i][j].email) {
                     todo += '<input value="0" name="r" type="checkbox" id="01">' +
-                        '<label htmlFor="01" style="color: #4f1c21;">' + mydata[i][j].email + '</label>';
+                        '<label htmlFor="01" style="color: #4f1c21;">' + mydata[i][j].email +'| '+ mydata[i][j].password + '| ' + mydata[i][j].comment +'</label>';
                 }
             }
 
