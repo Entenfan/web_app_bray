6fa4901b471fa76ed23bd82f19a4ddb508301c15
beluzzi <47862409+beluzzi@users.noreply.github.com>
2023-06-20 19:39:50 +0200

    Update main.js
    changed port from 80 to 8080

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 8080;
 const mysql = require('mysql');
 const execSync = require("child_process").execSync;
 
